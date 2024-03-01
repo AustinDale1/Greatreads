122810d1a0a04a85e4ec13e8081dfe60b8246a06
Austin Dale <ad25016@georgiasouthern.edu>
2024-03-01 13:02:01 -0500

    Initial Commit
    Figuring out structure and more importantly making sure my GitHub pages looks active please hire me

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styling/App.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>
+                <a href='https://github.com/AustinDale1'>Explore</a>
+                <a href='https://github.com/AustinDale1'>Friends</a>
+                <a href='https://github.com/AustinDale1'>Profile</a>
+            </h1>            
+
+        </div>
+    )
+}
+
+export default Header
